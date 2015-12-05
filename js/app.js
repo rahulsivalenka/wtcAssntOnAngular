0b8c711345d16b394630f059ee8e3cdfc759fb20
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2015-12-06 04:38:42 +0530

    created login view, its controller and configured its route

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 var app = angular.module(
   'EmployeeInfoApp', //module name
-  []
+  [
+    'ngRoute' //add route dependency to the module
+  ]
 ); //module
+
+//routing
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'LoginController',
+      templateUrl: 'views/login.html'
+    })
+    .otherwise({redirectTo: '/'});
+})
---
+++ js/controllers/LoginController.js
@@ -0,0 +1,9 @@
+app.controller('LoginController', [
+  //dependencies
+  '$scope', 
+  //controller logic
+  function($scope) {
+    $scope.user = {name: '', password: ''}
+    debugger;
+  }
+]);
