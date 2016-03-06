4cd92d1ad34edd0ea3445235268fa40774072f4a
Phani Rahul Sivalenka <rahul.sivalenka@walkingtree.in>
2016-03-07 01:31:32 +0530

    Added employee table with form and mock data for the table

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,5 +8,58 @@ app.controller('MainController', [
 
     /*models for the view*/
     $scope.params = $routeParams;
+    $scope.employee = {
+      name: {
+        firstName: 'Rahul',
+        lastName: 'SP'
+      },
+      id: null,
+      designation: '',
+      dob: new Date('3 July 1988'),
+      address: '',
+      mobile: '',
+      email: '',
+      addedBy: ''
+    };
+    $scope.employee.addedBy = $scope.loggedInUser.name;
+    $scope.employees = [];
+
+    $scope.employees = [{
+      name: {
+        firstName: 'Rahul',
+        lastName: 'SP'
+      },
+      id: null,
+      designation: '',
+      dob: new Date('3 July 1988'),
+      address: '',
+      mobile: '',
+      email: '',
+      addedBy: ''
+    }, {
+      name: {
+        firstName: 'Rahul',
+        lastName: 'SP2'
+      },
+      id: null,
+      designation: '',
+      dob: new Date('3 July 1988'),
+      address: 'Road No 8 Alkapuri, Hyderabad',
+      mobile: '',
+      email: '',
+      addedBy: ''
+    }, {
+      name: {
+        firstName: 'Rahul',
+        lastName: 'SP3'
+      },
+      id: null,
+      designation: '',
+      dob: new Date('3 July 1988'),
+      address: '',
+      mobile: '',
+      email: '',
+      addedBy: ''
+    }]
   }
 ]);
